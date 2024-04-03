968543777e207637569691523d976c7bcf809f7c
wizardy <ezhassan.info@gmail.com>
2024-04-03 11:01:56 +0000

    commit

--- code/main.lua
+++ code/main.lua
@@ -1,6 +1,23 @@
 print('Hello, World !') --this is just a line of code that says hello world!
+
 --[[
 --in lua
 --this is 
 --how we can create multiple lines of comments
 --]]
+
+name = "El azzouzi " .. "Hassan"
+
+print(name)
+
+--tests :
+print(7 == '7')
+
+
+--Functions in lua :
+
+function square(number)
+	return number*number
+end
+
+print(square(7))
