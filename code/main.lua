da51c2f4cd8324fcda5196c0bd364102cca3f556
wizardy <ezhassan.info@gmail.com>
2024-04-03 10:51:00 +0000

    commit

--- code/main.lua
+++ code/main.lua
@@ -1,1 +1,6 @@
-print('Hello, World !')
+print('Hello, World !') --this is just a line of code that says hello world!
+--[[
+--in lua
+--this is 
+--how we can create multiple lines of comments
+--]]
