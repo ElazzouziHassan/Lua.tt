012de9f3251d705aa16104f95b65af502fb44ebc
wizardy <ezhassan.info@gmail.com>
2024-04-03 11:38:46 +0000

    basic function in lua

---
+++ code/functions/main.lua
@@ -0,0 +1,7 @@
+function concatFullName(firstName, lastName)
+	return firstName .. " " .. lastName
+end
+
+fullName = concatFullName('Hassan', 'Elazzouzi')
+--uncomment bellow to print the results:
+--print(fullName)
